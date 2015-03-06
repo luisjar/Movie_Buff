cefb9c5689bbc602e2af51473ba0bd9849a93079
luisjar <luismigueljaramillo@hotmail.com>
2015-03-06 17:21:26 -0500

    Convert review ratings to stars

--- MovieBuff/db/schema.rb
+++ MovieBuff/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306210108) do
+ActiveRecord::Schema.define(version: 20150306211628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150306210108) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "movie_id"
   end
 
   create_table "users", force: true do |t|

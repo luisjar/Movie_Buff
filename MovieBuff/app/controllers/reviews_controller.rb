fc2472e2bffec52072f68fb42461c894885b88b1
luisjar <luismigueljaramillo@hotmail.com>
2015-03-15 21:35:46 -0500

    Add new review form into show movie page

--- MovieBuff/app/controllers/reviews_controller.rb
+++ MovieBuff/app/controllers/reviews_controller.rb
@@ -11,6 +11,18 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    # @movie = Movie.find(params[:movie_id])
+    # @review = Review.create(params[:review])
+    # @review.user_id = current_user_id
+    # @review.movie_id = @movie.id
+
+    # if @review.save
+    #   redirect_to movie_path(@movie)
+    # else
+    #   render 'new'
+    # end
+
+
     @review = Review.new(review_params)
     @review.user_id = current_user.id
     @review.movie_id = @movie.id
